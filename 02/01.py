c92969c8b5bf6325fcfff4f516454dfb02d28a68
vimparee <fasg901@mail.ru>
2022-06-25 12:28:56 +0500

    Урок 2
    опоздал, сильно

---
+++ 02/01.py
@@ -0,0 +1,7 @@
+city1 = input()
+city2 = input()
+if city1 == city2:
+    print('НЕТ')
+else:
+    print('ДА')
+
---
+++ 02/03.py
@@ -0,0 +1,8 @@
+days = 2
+lenght = 1843
+work = 4
+people = (lenght / (days * work))
+if int(people) < people:
+    print(int(people + 1))
+else:
+    print(int(people))
